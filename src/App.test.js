6dfe131123c2ffcbc1003dcd0a7ee0837c15362f
Andrei Sviazhin <asviazhin@apple.com>
2019-06-04 20:52:18 -0700

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, cleanup } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import "jest-dom/extend-expect";
+
+import App from "./App";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+it("renders welcome text", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("Welcome to React")).toBeInTheDocument();
 });
