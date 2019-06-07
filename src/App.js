91bf61a007bbe5f80f49404eecf955d3be5930f1
Andrei Sviazhin <asviazhin@apple.com>
2019-06-07 14:22:04 -0700

    Add GridLayout

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App container">
+        <header className="header">
+          <div className="App-header">
+            <h2>Header</h2>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+        </header>
+
+        <div className="body">
+          <h2>Body</h2>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload!!!!
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!!!!
-        </p>
+
+        <div className="sidebar">
+          <h2>Sidebar</h2>
+        </div>
+        <footer className="footer">
+          <h2>Footer</h2>
+        </footer>
       </div>
     );
   }
